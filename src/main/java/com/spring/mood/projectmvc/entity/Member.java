2d23e2d33faf72215cb5833fd2f12b5835581a8c
junglimit <jaehan_jjh@naver.com>
2024-06-14 17:18:33 +0900

    ✅Member 엔터티, mapper, mybatis 추가

---
+++ src/main/java/com/spring/mood/projectmvc/entity/Member.java
@@ -0,0 +1,28 @@
+package com.spring.mood.projectmvc.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Member {
+
+    private String userAccount; // PK
+
+    private int roomId; // ChatRooms FK
+
+    private String userRole;
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+    private LocalDateTime createAt;
+    private LocalDateTime userBirth;
+    private String userAddress;
+    private String userProfile;
+    private int userPoint;
+
+}
