bd99685dfa467ec30ffc562eef740a8f19599b8c
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-14 14:52:51 +0900

    ✅ 초기파일

--- src/main/java/com/spring/mood/projectmvc/controller/restApiController/chatRestApiController/ChatApiController.java
+++ src/main/java/com/spring/mood/projectmvc/controller/restApiController/chatRestApiController/ChatApiController.java
@@ -1,10 +1,9 @@
-package com.spring.mood.projectmvc.controller.controller.chatRestApiController;
+package com.spring.mood.projectmvc.controller.restApiController.chatRestApiController;
 
 
 import com.spring.mood.projectmvc.entity.ChatEntity;
 import com.spring.mood.projectmvc.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
