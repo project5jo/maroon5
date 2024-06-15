8c0f17134d2825d02253a9b2bceedff91a8b6deb
junglimit <jaehan_jjh@naver.com>
2024-06-15 14:52:24 +0900

    ✅ Orders , ShopItem 엔터티 추가

---
+++ src/main/java/com/spring/mood/projectmvc/entity/ShopItem.java
@@ -0,0 +1,24 @@
+package com.spring.mood.projectmvc.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+// 숫자타입 전부 어떤걸로 통일할지 검토필요
+public class ShopItem {
+    private Long shopItemId; // PK
+    private String shopItemName;
+    private String shopItemDesc;
+    private Double shopItemPrice; // decimal type임 수정검토
+    private String shopItemImg;
+    private LocalDateTime shopItemDate;
+    private Long shopItemStock;
+    private Long shopItemView;
+}
