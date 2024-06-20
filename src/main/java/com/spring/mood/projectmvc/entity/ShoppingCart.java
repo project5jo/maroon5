1eba0ce6fa1f90daf7aa934df5e88cf71d65ed6e
Jehoon <limjhoon8@gmail.com>
2024-06-20 14:47:51 +0900

    🐛 실행 오류 수정

--- src/main/java/com/spring/mood/projectmvc/entity/ShoppingCart.java
+++ src/main/java/com/spring/mood/projectmvc/entity/ShoppingCart.java
@@ -1,29 +1,29 @@
-package com.spring.mood.projectmvc.entity;
-
-import lombok.*;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Getter
-@ToString
-@EqualsAndHashCode
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-public class ShoppingCart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long cartId;
-
-    private Double cartTotalPrice;
-    private long cartTotalCount;
-
-    @ManyToOne
-    @JoinColumn(name = "user_account")
-    private Member member;
-
-    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ShopItem> items;
-}
+//package com.spring.mood.projectmvc.entity;
+//
+//import lombok.*;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Getter
+//@ToString
+//@EqualsAndHashCode
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//@Entity
+//public class ShoppingCart {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long cartId;
+//
+//    private Double cartTotalPrice;
+//    private long cartTotalCount;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_account")
+//    private Member member;
+//
+//    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<ShopItem> items;
+//}
