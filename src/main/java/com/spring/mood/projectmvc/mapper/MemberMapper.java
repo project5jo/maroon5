6dc81048f1de0e4731f5a7f6f77f6a5cdb4838af
yaejin12 <pos03167@naver.com>
2024-06-20 17:41:08 +0900

    자동로그인 완료

--- src/main/java/com/spring/mood/projectmvc/mapper/MemberMapper.java
+++ src/main/java/com/spring/mood/projectmvc/mapper/MemberMapper.java
@@ -1,5 +1,6 @@
 package com.spring.mood.projectmvc.mapper;
 
+import com.spring.mood.projectmvc.dto.requestDto.AutoSignInDto;
 import com.spring.mood.projectmvc.entity.Member;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -15,4 +16,10 @@ public interface MemberMapper {
     boolean checkId(String account);
 
     boolean checkEmail(String email);
+
+    //자동로그인 쿠키값, 만료시간 업데이트
+    void updateAutoLogin(AutoSignInDto dto);
+
+    //세션아이디로 회원정보 조회
+    Member findUserBySessionId (String sessionId);
 }
