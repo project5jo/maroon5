4a57c4afd7c5d4573ce2b7bba2100a3aa893037f
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-28 12:06:54 +0900

    ✅ 마이페이지 연결 조건 추가

--- src/main/java/com/spring/mood/projectmvc/controller/controller/MyPageController.java
+++ src/main/java/com/spring/mood/projectmvc/controller/controller/MyPageController.java
@@ -5,7 +5,10 @@ import com.spring.mood.projectmvc.dto.responseDto.SignInUserInfoDTO;
 import com.spring.mood.projectmvc.entity.Member;
 import com.spring.mood.projectmvc.service.MyPageService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,6 +18,7 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 public class MyPageController {
 
+    private static final Logger log = LoggerFactory.getLogger(MyPageController.class);
     MyPageService service;
 
     // MyPage 클릭했을 때 조건에 맞는경우 웹페이지 띄우기
@@ -31,7 +35,23 @@ public class MyPageController {
     }
 
     @GetMapping("/mypage-memberinfo")
-    public String openMyPageMemberInfo () {
+    public String openMyPageMemberInfo (HttpSession session, Model model) {
+
+        // 세션에서 로그인한 회원의 정보를 가져오기
+        SignInUserInfoDTO loginUser = (SignInUserInfoDTO) session.getAttribute("loginUser");
+        // 로그인한 회원의 정보에서 아이디 가져오기
+        String account = loginUser.getAccount();
+//
+////        // 아이디를 통해 해당 회원 한 명의 정보 찾기
+//        RequestMyPageMemberInfoDto thisMember = service.serviceFindOne(account);
+//
+//        Member member = membermapper.findOne(account);
+
+//        String profile = member.getUserProfile();
+
+        // 회원의 정보를 JSP 로 보내기
+//        model.addAttribute("thisMember", thisMember);
+
         return "html/mypage-memberInfo";
     }
 
--- src/main/java/com/spring/mood/projectmvc/service/MyPageService.java
+++ src/main/java/com/spring/mood/projectmvc/service/MyPageService.java
@@ -1,8 +1,28 @@
 package com.spring.mood.projectmvc.service;
 
+import com.spring.mood.projectmvc.dto.requestDto.RequestMyPageMemberInfoDto;
+import com.spring.mood.projectmvc.entity.Member;
+import com.spring.mood.projectmvc.mapper.MemberMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyPageService {
 
+    MemberMapper membermapper;
+
+//    public RequestMyPageMemberInfoDto serviceFindOne(String account) {
+//
+//        Member member = membermapper.findOne(account);
+//
+//        String profile = member.getUserProfile();
+//
+////        System.out.println("멤버나오니?" + member);
+////
+////        RequestMyPageMemberInfoDto dto = new RequestMyPageMemberInfoDto(member);
+//
+////        if (member.getUserProfile())
+////        System.out.println("디티오나오니?" + dto);
+//
+//        return  dto;
+//    }
 }
