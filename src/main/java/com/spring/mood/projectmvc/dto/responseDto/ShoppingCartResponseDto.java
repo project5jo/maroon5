66847e30e488bcd5e1fc2a930d5e4e2f730d8057
junglimit <jaehan_jjh@naver.com>
2024-06-29 17:03:46 +0900

    ✅  쇼핑카트 수량조절과 조절시 품목값, 토탈값적용, CSS수정

--- src/main/java/com/spring/mood/projectmvc/dto/responseDto/ShoppingCartResponseDto.java
+++ src/main/java/com/spring/mood/projectmvc/dto/responseDto/ShoppingCartResponseDto.java
@@ -4,6 +4,7 @@ import com.spring.mood.projectmvc.entity.ShoppingCart;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Getter
 @ToString
@@ -19,9 +20,11 @@ public class ShoppingCartResponseDto {
     private String shopItemName;
     private String shopItemDesc;
     private String shopItemImg;
+    private BigDecimal unitPrice;  // 추가된 필드
 
     // ShoppingCart 엔티티로부터 ShoppingCartResponseDto 생성
     public static ShoppingCartResponseDto fromEntity(ShoppingCart cart) {
+        BigDecimal unitPrice = cart.getCartTotalPrice().divide(new BigDecimal(cart.getCartTotalCount()), 2, RoundingMode.HALF_UP);
         return ShoppingCartResponseDto.builder()
                 .cartId(cart.getCartId())
                 .cartTotalPrice(cart.getCartTotalPrice())
@@ -31,6 +34,7 @@ public class ShoppingCartResponseDto {
                 .shopItemName(cart.getShopItemName())
                 .shopItemDesc(cart.getShopItemDesc())
                 .shopItemImg(cart.getShopItemImg())
+                .unitPrice(unitPrice)
                 .build();
     }
 }
--- src/main/java/com/spring/mood/projectmvc/mapper/ShoppingCartMapper.java
+++ src/main/java/com/spring/mood/projectmvc/mapper/ShoppingCartMapper.java
@@ -1,19 +1,23 @@
 package com.spring.mood.projectmvc.mapper;
 
-import com.spring.mood.projectmvc.entity.ShopItem;
 import com.spring.mood.projectmvc.entity.ShoppingCart;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 @Mapper
 public interface ShoppingCartMapper {
 
-    List<ShoppingCart> findByUserAccount(String userAccount);
-    // 장바구니 아이템 조회 메서드 추가
-    List<ShopItem> findItemsByCartId(@Param("cartId") Long cartId);
-    // 장바구니에 상품 추가
+    List<ShoppingCart> findByUserAccount(@Param("userAccount") String userAccount);
+
+    @Update("UPDATE ShoppingCart SET cart_total_price = #{cartTotalPrice}, cart_total_count = #{cartTotalCount} WHERE cart_id = #{cartId}")
+    boolean update(ShoppingCart shoppingCart);
+
+    @Select("SELECT * FROM ShoppingCart WHERE user_account = #{userAccount} AND shop_item_id = #{itemId}")
+    ShoppingCart findItemByUserAccountAndItemId(@Param("userAccount") String userAccount, @Param("itemId") Long itemId);
+
     boolean save(ShoppingCart shoppingCart);
 }
